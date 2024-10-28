e11eeb9087fd55a47d042e18d4164ef3086d04ad
jun <an0yjun@naver.com>
2024-10-28 23:46:29 +0900

    zuul service설정

--- zuul-service/src/main/java/com/zuulservice/filter/ZuulLoggingFilter.java
+++ zuul-service/src/main/java/com/zuulservice/filter/ZuulLoggingFilter.java
@@ -4,16 +4,18 @@ import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 
 import javax.servlet.http.HttpServletRequest;
 
+@Component
 public class ZuulLoggingFilter extends ZuulFilter {
     org.slf4j.Logger logger = LoggerFactory.getLogger(ZuulLoggingFilter.class);
 
     @Override
     public boolean shouldFilter() {
-        return false;
+        return true;
     }
 
     @Override
