401f2a92ff503e5c4d10e95ccf982157a4c66219
0jun <an0yjun@gmail.com>
2024-10-29 12:52:43 +0900

    loggingfilter add

---
+++ apigateway-service/src/main/java/com/apigatewayservice/config/LoggingFilter.java
@@ -0,0 +1,58 @@
+package com.apigatewayservice.config;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Config> {
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return ((exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+            log.info("logging filter baseMessage: " + config.getBaseMessage());
+            if(config.isPreLogger()){
+                log.info("logging pre Filter: request uri: " + request.getURI());
+            }
+            return chain.filter(exchange).then(Mono.fromRunnable(()->{
+                if(config.isPostLogger()){
+                    log.info("logging post Filter: response code " + response.getStatusCode());
+                }
+            }));
+        });
+
+//        GatewayFilter filter = new OrderedGatewayFilter((exchange, chain)->{
+//            ServerHttpRequest request = exchange.getRequest();
+//            ServerHttpResponse response = exchange.getResponse();
+//            log.info("logging filter baseMessage: " + config.getBaseMessage());
+//
+//            if(config.isPreLogger()){
+//                log.info("logging pre Filter: request uri: " + request.getURI());
+//            }
+//            return chain.filter(exchange).then(Mono.fromRunnable(()->{
+//                if(config.isPostLogger()){
+//                    log.info("logging post Filter: response code " + response.getStatusCode());
+//                }
+//            }));
+//        }, Ordered.HIGHEST_PRECEDENCE);
+//        return filter;
+    }
+
+    @Data
+    public class Config{
+        private String baseMessage;
+        private boolean preLogger;
+        private boolean postLogger;
+
+    }
+}
